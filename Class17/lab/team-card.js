f3f5425b49a95c1c2b2c2447f3217b2527d09b30
Rama-Chaker <rama.chaker1@gmail.com>
2026-06-18 13:33:22 -0400

    Created team card.js

---
+++ Class17/lab/team-card.js
@@ -0,0 +1,76 @@
+class TeamCard extends HTMLElement {
+    connectedCallback() {
+        this.render();
+    }
+    getName() {
+        return this.getAttribute("name");
+    }
+    getGroup() {
+        return this.getAttribute("group");
+    }
+    getPoints() {
+        return this.getAttribute("points");
+    }
+    getPlayed() {
+        return this.getAttribute("played");
+    }
+    getGoalDifference() {
+        return this.getAttribute("goal-difference");
+    }
+
+    renderStyle() {
+        return `
+            <style>
+                .team-card {
+                    border: 1px solid #ccc;
+                    border-radius: 8px;
+                    padding: 16px;
+                    max-width: 300px;
+                    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+                    font-family: Arial, sans-serif;
+                }                
+                .team-poster {
+                width: 100%;
+                height: auto;
+                border-radius: 8px;
+                 }
+                .team-title {
+                    font-size: 1.5em;
+                    font-weight: bold;
+                    margin-bottom: 8px;
+                }
+                .team-info {
+                    font-size: 1em;
+                    margin-bottom: 8px;
+                }
+                .details-button {
+                    background-color: #007BFF;  
+                    color: white;
+                    border: none;
+                    padding: 10px 20px;
+                    border-radius: 4px;
+                }
+                .details-button:hover {
+                    background-color: #0056b3;  
+                }
+                
+            </style>
+          
+        `;
+    }
+    render() {
+        const shadow = this.attachShadow({ mode: "open" });
+        shadow.innerHTML = `
+        ${this.renderStyle()}
+        <div class="team-card">
+            <h2 class="team-title">${this.getName()}</h2>
+            <p class="team-info">Group: ${this.getGroup()}</p>
+            <p class="team-info">Points: ${this.getPoints()}</p>
+            <p class="team-info">Played: ${this.getPlayed()}</p>
+            <p class="team-info">Goal Difference: ${this.getGoalDifference()}</p>
+            <button class="details-button">View Details</button>
+        </div>
+     `;
+    }
+}
+customElements.define("team-card", TeamCard);
